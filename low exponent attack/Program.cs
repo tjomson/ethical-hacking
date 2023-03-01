e8784de1f2e9e795635ea873f89cae1b1d7df078
Thomas Hoffmann Kilbak <thomaskilbak@gmail.com>
2023-03-01 15:51:32 +0100

    Fix submission

--- low exponent attack/Program.cs
+++ low exponent attack/Program.cs
@@ -1,49 +1,28 @@
-﻿// Tyvstjålet fra tilfældige folk på nettet som chatgpt har læst om
-using System.Numerics;
+﻿using System.Numerics;
 
 class Program
 {
     static void Main()
     {
-        BigInteger x = BigInteger.Parse("1050121080167272443556048214450867363441054732447557600854574323371021993341233414471786819540311637578524782318090701401471864654252154832829029374517109232106237855161080452403809667134710975656071552494599424506891661877234383682734813095252516341");
-        int n = 7;
-        int precision = 200;
+        BigInteger n = BigInteger.Parse("1050121080167272443556048214450867363441054732447557600854574323371021993341233414471786819540311637578524782318090701401471864654252154832829029374517109232106237855161080452403809667134710975656071552494599424506891661877234383682734813095252516341");
+        int e = 7;
 
-        // Approximate the 7th root by raising x to an integer power
-        BigInteger root = BigInteger.Pow(x, 1 / n);
+        BigInteger root = BigInteger.Pow(n, 1 / e);
 
-        // Refine the approximation using binary search
-        BigInteger upperBound = BigInteger.Pow(10, precision);
-        BigInteger lowerBound = BigInteger.Zero;
+        BigInteger high = n;
+        BigInteger low = 0;
 
-        while (true)
+        while (high > low)
         {
-            // Calculate the midpoint between the upper and lower bounds
-            BigInteger mid = (upperBound + lowerBound) / 2;
-
-            // Calculate the nth power of the midpoint
-            BigInteger midPower = BigInteger.Pow(mid, n);
-
-            // If the nth power is too large, move the upper bound down
-            if (midPower > x)
-            {
-                upperBound = mid;
-            }
-            // If the nth power is too small, move the lower bound up
-            else if (midPower < x)
-            {
-                lowerBound = mid;
-            }
-            // If the nth power is exact, we've found the root
+            BigInteger mid = (high + low) / 2;
+            BigInteger midPower = BigInteger.Pow(mid, e);
+            if (midPower > n)
+                high = mid;
+            else if (midPower < n)
+                low = mid;
             else
             {
                 root = mid;
-                break;
-            }
-
-            // If the bounds are next to each other, we've reached the desired precision
-            if (upperBound - lowerBound <= 1)
-            {
                 break;
             }
         }
--- low exponent attack/solution.md
+++ low exponent attack/solution.md
@@ -21,19 +21,45 @@ The problem is therefor a combination of the fact that a small e has been chosen
 
 ## Code to solve
 
-A python program that does this, could like this:
-```python
-c = 1050...
-e = 7
-res = pow(c, 1/e)
-bytes = int(res).to_bytes(4096, 'big')
-decoded = bytes.decode('utf-8')
-print(decoded)
+Calculating the 7th root is trivial with small numbers, but to do so for big numbers where accuracy is also required, I can binary search from 0 to n and find the value `a` that satisfies `a^e = n`
+
+A C# program that does this, could like this:
+```csharp
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        BigInteger n = BigInteger.Parse("1050121080167272443556048214450867363441054732447557600854574323371021993341233414471786819540311637578524782318090701401471864654252154832829029374517109232106237855161080452403809667134710975656071552494599424506891661877234383682734813095252516341");
+        int e = 7;
+
+        BigInteger root = BigInteger.Pow(n, 1 / e);
+
+        BigInteger high = n;
+        BigInteger low = 0;
+
+        while (high > low)
+        {
+            BigInteger mid = (high + low) / 2;
+            BigInteger midPower = BigInteger.Pow(mid, e);
+            if (midPower > n)
+                high = mid;
+            else if (midPower < n)
+                low = mid;
+            else
+            {
+                root = mid;
+                break;
+            }
+        }
+        var bytes = root.ToByteArray();
+        Array.Reverse(bytes);
+        var str = System.Text.Encoding.UTF8.GetString(bytes);
+        System.Console.WriteLine(str);
+    }
+}
 ```
-
-This code does not work entirely though, as python does not use enough decimal places to achieve enough precision. This yields the result `HKN{b...` which is the start of the result, but due to missing preceision in the number, there are missing bytes of accuracy.
-
-To fix this, one can use a calculator with high precision, or use binary search to search the range 0 to n for a value x where `x^7 = n`.
 
 ## Other problems with RSA
 
